78f1e63306b87ec752a71f4a2409798f1e1c05b0
Alex <baehs1989@gmail.com>
2020-07-24 00:32:52 -0400

    slidewhow updates

--- src/components/SlideShow/SlideShow.js
+++ src/components/SlideShow/SlideShow.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
+import styles from './SlideShow.module.css'
 
 const fadeProperties = {
-    duration: 5000,
+    duration: 8000,
+    transitionDuration:1200,
     pauseOnHover: true
 };
 
 export default function SlideShow() {
     return (
-        <div className="slide-container">
+        <div className={styles.SlideContainer}>
           <Slide {...fadeProperties}>
-            <div className="each-slide">
-              <div style={{width:'100%', height:'400px'}}>
-                <span>Slide 1</span>
-              </div>
+            <div className={styles.Slide}>
+                <div>
+                    <span>Slide 1</span>
+                </div>
             </div>
-            <div className="each-slide">
-                <div style={{width:'100%', height:'400px'}}>
-                <span>Slide 2</span>
-              </div>
+            <div className={styles.Slide}>
+                <div>
+                    <span>Slide 2</span>
+                </div>
             </div>
-            <div className="each-slide">
-                <div style={{width:'100%', height:'400px'}}>
-                <span>Slide 3</span>
-              </div>
+            <div className={styles.Slide}>
+                <div>
+                    <span>Slide 3</span>
+                </div>
             </div>
           </Slide>
         </div>
