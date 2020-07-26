e50ecc3472ea657e4b36e08d0c167430aaba5a00
Alex <baehs1989@gmail.com>
2020-07-26 15:38:33 -0400

    mypoint added

--- src/components/MyPage/MyPoint/MyPoint.js
+++ src/components/MyPage/MyPoint/MyPoint.js
@@ -1,11 +1,19 @@
 import React from 'react'
+
 import PageContainer from '../../../hoc/PageContainer'
+import styles from './MyPoint.module.css'
 
 export default function MyPoint() {
     return (
         <PageContainer>
-            <div>
-                Hello
+            <div className={styles.Container}>
+                <div className={styles.InnerContainer}>
+                    <div className={styles.Header}>Points</div>
+                    <div>
+                        <span className={styles.Title}>Available Points: </span> <span className={styles.Point}>{23}</span>
+                    </div>
+                </div>
+
             </div>
         </PageContainer>
 
