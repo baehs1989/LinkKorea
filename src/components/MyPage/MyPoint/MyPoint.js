afa6beb9178999809a5795e4a9a811ac9d915e05
Alex <baehs1989@gmail.com>
2020-07-27 00:48:17 -0400

    points page updated

--- src/components/MyPage/MyPoint/MyPoint.js
+++ src/components/MyPage/MyPoint/MyPoint.js
@@ -6,7 +6,7 @@ import styles from './MyPoint.module.css'
 export default function MyPoint() {
     return (
         <PageContainer>
-            <div className={styles.Container}>
+            {/* <div className={styles.Container}>
                 <div className={styles.InnerContainer}>
                     <div className={styles.Header}>Points</div>
                     <div>
@@ -14,7 +14,28 @@ export default function MyPoint() {
                     </div>
                 </div>
 
+            </div> */}
+
+            <div className={styles.box}>
+                <div className={styles.card}>
+                    <div className={styles.bank}>
+                        LinkKorea
+                    </div>
+                    <div className={styles.chip}></div>
+                    <div className={styles.number}>
+                        Customer Number : 123
+                    </div>
+                    <div className={styles.date_box}>
+                        <div className={styles.valid}>Available Points</div>
+                        <div className={styles.date}>12</div>
+                    </div>
+                    <div className={styles.card__footer}>
+                        <div className={styles.name}>Hyung Soo Bae</div>
+                        <img className={styles.logo} src="https://svgontheweb.com/assets/img/bblogo-optim2.svg" alt="logo"></img>
+                    </div>
+                </div>
             </div>
+
         </PageContainer>
 
     )
